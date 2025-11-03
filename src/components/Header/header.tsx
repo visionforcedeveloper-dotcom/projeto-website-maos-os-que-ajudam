cc4cbba802f69e9104fe6e7382e5afc70a822887
vnfontes <viniciusfontes02@gmail.com>
2025-11-03 12:11:21 -0300

    header-commit

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,20 @@
+import styles from './header.module.css'
+
+export function Header (){
+    return (
+        <header>
+            <nav className={styles.navbar}>
+                <div className="logo">
+                    <img src="/assets/logo-projeto.png" alt="Logo Mãos Que Ajudam"/>
+                </div>
+
+                <ul className={styles['nav-links']}>
+                    <li><a href="">Sobre Nós</a></li>
+                    <li><a href="">Projetos</a></li>
+                    <li><a href="">Fale Conosco</a></li>
+                </ul>
+
+            </nav>
+        </header>
+    )
+}
