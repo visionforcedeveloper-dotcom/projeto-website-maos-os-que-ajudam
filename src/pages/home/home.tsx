fa6c60f5408395b991573b959377d39c5ce7240a
Aluno <aluno@pedagogico.CEUMA.EDU.BR>
2025-11-05 11:02:02 -0300

    sobre nos finalizada

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,12 @@
+
+import SobreNos from '../sobre/sobre'
 import styles from './home.module.css'
 
 export function HomePage(){
     return(
-        <h1 className={styles.h1}>eu sou Home</h1>
+        <>
+ 
+        <SobreNos />
+        </>
     )
 }
