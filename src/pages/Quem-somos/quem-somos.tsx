4b1ced0984890c72cd527621abce499f5689d45e
vitoria.araujo <vitoria.araujo@ceuma.br>
2025-11-06 11:41:07 -0300

    refact: refatoração de estrutura do componenete

--- src/pages/Quem-somos/quem-somos.tsx
+++ src/pages/Quem-somos/quem-somos.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import styles from "./Quem-somos.module.css"
+import styles from "./quem-somos.module.css";
 import { MdAttachMoney } from "react-icons/md";
 import { FaGift } from "react-icons/fa6";
 import { CiClock2 } from "react-icons/ci";
 import { GoPaperAirplane } from "react-icons/go";
 
-export const ComoAjudar: React.FC = () => {
+export function ComoAjudar() {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>Como Você Pode Ajudar</h2>
@@ -17,11 +16,9 @@ export const ComoAjudar: React.FC = () => {
       <div className={styles.cards}>
         <div className={styles.card}>
           <div className={styles.icon}>
-
             <MdAttachMoney size={35} />
-
           </div>
-        
+
           <h3>Doe</h3>
           <p>
             Contribuições financeiras nos ajudam a manter nossos programas e
@@ -31,8 +28,8 @@ export const ComoAjudar: React.FC = () => {
 
         <div className={styles.card}>
           <div className={styles.icon}>
-         <FaGift size={20}/>
-         </div>
+            <FaGift size={20} />
+          </div>
 
           <h3>Doe Itens</h3>
           <p>
@@ -43,9 +40,9 @@ export const ComoAjudar: React.FC = () => {
 
         <div className={styles.card}>
           <div className={styles.icon}>
-            <CiClock2 size={28}/>
+            <CiClock2 size={28} />
           </div>
-         
+
           <h3>Seja Voluntário</h3>
           <p>
             Doe seu tempo e talento ajudando em nossas atividades e programas.
@@ -54,9 +51,9 @@ export const ComoAjudar: React.FC = () => {
 
         <div className={styles.card}>
           <div className={styles.icon}>
-            <GoPaperAirplane size={24}/>
+            <GoPaperAirplane size={24} />
           </div>
-        
+
           <h3>Divulgue</h3>
           <p>
             Compartilhe nossas ações nas redes sociais e ajude a alcançar mais
@@ -65,27 +62,23 @@ export const ComoAjudar: React.FC = () => {
         </div>
       </div>
 
-      <button  className={styles.button}>Faça Sua Doação Agora</button>
-<div className={styles.whapper}> 
-
-      <div className={styles.contact}>
-        <img
-          src="assets/logo-projeto.png"
-          alt="Logo-projeto"
-          className={styles.logo}
-      />
-        <h3>Fale Conosco</h3>
-        <p>
-          <strong>Endereço:</strong>  R. Treze de Maio, 10 - João de Deus
-        </p>
-        <p>
-          <strong>Telefone/WhatsApp:</strong> (98) 9 9904-3539
-        </p>
-        <p>
-        </p>
+      <button className={styles.button}>Faça Sua Doação Agora</button>
+      <div className={styles.whapper}>
+        <div className={styles.contact}>
+          <img
+            src="assets/logo-projeto.png"
+            alt="Logo-projeto"
+            className={styles.logo}
+          />
+          <h3>Fale Conosco</h3>
+          <p>
+            <strong>Endereço:</strong> R. Treze de Maio, 10 - João de Deus
+          </p>
+          <p>
+            <strong>Telefone/WhatsApp:</strong> (98) 9 9904-3539
+          </p>
+        </div>
       </div>
-</div>
     </section>
   );
-};
-
+}
