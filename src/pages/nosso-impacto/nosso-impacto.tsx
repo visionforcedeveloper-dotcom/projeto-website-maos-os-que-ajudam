4667339d008678974d180ff18f9a71f70412fda0
vnfontes <viniciusfontes02@gmail.com>
2025-11-10 11:40:02 -0300

    wip: criando seçao de nosso impacto

---
+++ src/pages/nosso-impacto/nosso-impacto.tsx
@@ -0,0 +1,22 @@
+import styles from "./impacto.module.css";
+
+export function NossoImpacto() {
+  return (
+    <section>
+      <div className={styles.container}>
+        <div>
+          <h1>Nosso Impacto</h1>
+        </div>
+
+        <div>
+          <h2>40</h2>
+          <h3>Familía Cadastradas</h3>
+          <p>
+            Atendimento direto e personalizado para cada núcleo familiar em
+            vulnerabilidade
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
