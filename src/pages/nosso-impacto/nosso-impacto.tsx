20cf153ed93918c0d61729b15e37c6d2fcded44e
vnfontes <viniciusfontes02@gmail.com>
2025-11-12 10:48:51 -0300

    nosso-impacto-finalizado

--- src/pages/nosso-impacto/nosso-impacto.tsx
+++ src/pages/nosso-impacto/nosso-impacto.tsx
@@ -3,34 +3,28 @@ import styles from "./nosso-impacto.module.css";
 export function NossoImpacto() {
   return (
     <section>
+      <h1>Nosso Impacto</h1>
       <div className={styles.container}>
-        <h1 className={styles.tlite}>Nosso Impacto</h1> 
-        <div className={styles.box}>
-          <h2>40</h2>
-          <h3>Familía Cadastradas</h3>
-          <p>
+        <div className={styles.card}>
+          <h2 className={styles.title}>40</h2>
+          <h3 className={styles.subtitle}>Familía Cadastradas</h3>
+          <p className={styles.description}>
             Atendimento direto e personalizado para cada núcleo familiar em
             vulnerabilidade
           </p>
         </div>
-      </div>
-
-      <div className={styles.container}>
-        <div className={styles.box}>
-          <h2>100%</h2>
-          <h3>Doações Empresariais</h3>
-          <p>
+        <div className={styles.card}>
+          <h2 className={styles.title}>100%</h2>
+          <h3 className={styles.subtitle}>Doações Empresarias</h3>
+          <p className={styles.description}>
             Cestas básicas fornecidas através de parcerias estratégicas com
             empresas locais
           </p>
         </div>
-      </div>
-
-      <div className={styles.container}>
-        <div className={styles.box}>
-          <h2>3</h2>
-          <h3>Áreas de Apoio</h3>
-          <p>
+        <div className={styles.card}>
+          <h2 className={styles.title}>3</h2>
+          <h3 className={styles.subtitle}>Áreas de Apoio</h3>
+          <p className={styles.description}>
             Atendimento multiplicinar: nutricional, júridico e psicoóogico
           </p>
         </div>
